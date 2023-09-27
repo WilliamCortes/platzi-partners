d51f43750f8781bc0bcf6ac4c5ce93fed2f04b74
WilliamCortesT <wcortes@talenta.cloud>
2023-09-26 20:18:39 -0500

    fix: console the error

--- pages/index.tsx
+++ pages/index.tsx
@@ -26,7 +26,13 @@ export const getStaticProps: GetStaticProps = async () => {
         }
       };
     } else {
-      throw new Error(`Connection limit reached. Please try again later.`);
+      console.error('🚀 ~ file: index.tsx *****************', { error: e });
+      // throw new Error(`Connection limit reached. Please try again later.`);
+      return {
+        props: {
+          clusterStillProvisioning: true
+        }
+      };
     }
   }
 
