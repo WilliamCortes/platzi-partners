e0c4952b737c31124976dcf48a63b1f3128d90e5
WilliamCortesT <wcortes@talenta.cloud>
2023-10-06 15:43:27 -0500

    feat: custom meta data

--- components/layout/meta.tsx
+++ components/layout/meta.tsx
@@ -1,11 +1,11 @@
 import Head from 'next/head';
 
 export const defaultMetaProps = {
-  title: 'MongoDB Starter Kit',
-  description:
-    'MongoDB Starter Kit built with Next.js, Vercel, and MongoDB Atlas.',
-  ogImage: `https://assets.vercel.com/image/upload/v1654626375/twitter-cards/mongo-integration-starter.png`,
-  ogUrl: 'https://mongodb.vercel.app'
+  title: 'Platzi Partners',
+  description: 'This is the place to search for the best that you are.',
+  ogImage:
+    'https://pps.whatsapp.net/v/t61.24694-24/376153218_812395623702104_3901179906043444054_n.jpg?ccb=11-4&oh=01_AdTrqx7iZWZu0FRVqsjEQRjnYb_-UhQys1MsENlBikPSXQ&oe=652D78AA&_nc_sid=000000&_nc_cat=110',
+  ogUrl: 'https://www.platzi.pro/'
 };
 
 export interface MetaProps {
@@ -22,6 +22,7 @@ export default function Meta({ props }: { props: MetaProps }) {
       <link rel="icon" href="/favicon.ico" />
       <link rel="shortcut icon" type="image/x-icon" href="/favicon.ico" />
       <link rel="apple-touch-icon" sizes="180x180" href="/favicon.ico" />
+      <link rel="canonical" href={props?.ogUrl} />
       <meta name="theme-color" content="#7b46f6" />
 
       <meta charSet="utf-8" />
@@ -35,7 +36,11 @@ export default function Meta({ props }: { props: MetaProps }) {
       <meta property="og:description" content={props?.description} />
       <meta property="og:url" content={props?.ogUrl} />
       <meta property="og:image" content={props?.ogImage} />
+      <meta property="og:image:alt" content={props?.title} />
       <meta property="og:type" content="website" />
+      <meta property="og:site_name" content={props?.title} />
+      <meta property="og:image:width" content="500" />
+      <meta property="og:image:height" content="500" />
 
       <meta name="twitter:card" content="summary_large_image" />
       <meta name="twitter:site" content="@Vercel" />
